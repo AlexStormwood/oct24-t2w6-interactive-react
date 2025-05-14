b59835fb87bb67ec952e028f6ba507bcbba1c193
Alex <alex.holder@bigfootds.com>
2025-05-14 20:00:56 +1000

    feat: interactivity testing wip

--- tests/PokemonSearcher.test.jsx
+++ tests/PokemonSearcher.test.jsx
@@ -1,6 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 import { PokemonSearcher } from "../src/components/PokemonSearcher";
+import userEvent from "@testing-library/user-event";
 
 
 describe("PokemonSearcher component tests", () => {
@@ -22,5 +23,35 @@ describe("PokemonSearcher component tests", () => {
 		// render(<PokemonSearcher />)
 
 	});
+
+
+	test("Can type in Pokemon name and search for that provided name", async () => {
+
+		const result = render(<PokemonSearcher />);
+
+		// Find the text input field for the pokemon name
+		const pokemonSearchNameInputElement = screen.getByTestId("pokemonNameInput");
+		expect(pokemonSearchNameInputElement).toBeInTheDocument();
+
+		const user = userEvent.setup();
+
+		// await user.click(pokemonSearchNameInputElement);
+		// await user.keyboard(pokemonSearchNameInputElement,"pikachu");
+
+		await user.type(pokemonSearchNameInputElement,"pikachu");
+
+		// expect(pokemonSearchNameInputElement.innerText).toBe("pikachu");
+		
+		waitFor(() => {
+			expect(pokemonSearchNameInputElement.value).toBe("pikachu");
+		});
+
+		// Find the button to submit the pokemon name to the API 
+
+		// Find the content rendered to the page to reflect the API result 
+
+	});
+
+
 
 });
